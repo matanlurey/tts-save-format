a9d23f1e5842b2c427cd83e68d54f38d4d407057
Matan Lurey <matanlurey@users.noreply.github.com>
2020-05-05 15:48:43 -0700

    More work towards a functioning package (#1)
    * Cleanup the generated code.
    
    * Fix schemas, add validation tests.
    
    * Sanity test against a large mod, and make some changes.
    
    * Some tweaks.
    
    * Fix lints.
    
    * Format.
    
    * Fix case-sensitive naming.
    
    * Use JavaScript's sorting, not the OS, to determine generation order.

---
+++ tool/generate.ts
@@ -0,0 +1,55 @@
+import * as fs from 'fs-extra';
+import { compileFromFile } from 'json-schema-to-typescript';
+import minimist from 'minimist';
+import * as path from 'path';
+import prettier from 'prettier';
+
+const inSchema = path.join('src', 'schema');
+const outTypes = path.join('src', 'types.d.ts');
+
+const argv = minimist(process.argv.slice(2));
+
+let output = '';
+
+const writeFile = argv.output
+  ? (text: string): void => fs.writeFileSync(outTypes, text)
+  : (text: string): void => {
+      output = `${text}`;
+    };
+
+const appendFile = argv.output
+  ? (text: string): void => fs.appendFileSync(outTypes, text)
+  : (text: string): void => {
+      output += text;
+    };
+
+export async function compileSchemas(): Promise<string> {
+  writeFile(
+    '/* tslint:disable */\n/** Execute `npm run generate` to regenerate **/',
+  );
+  const schemaFiles = fs.readdirSync(inSchema);
+  schemaFiles.sort();
+  for (const schema of schemaFiles) {
+    let out = await compileFromFile(path.join(inSchema, schema), {
+      cwd: inSchema,
+      declareExternallyReferenced: false,
+      bannerComment: `/* Generated from ${schema} */`,
+    });
+    // Some of the referenced types are suffixed with numbers, probably due to
+    // not canonicalizing references in the schema. This is a huge hack, but
+    // is probably stable - it removes trailing numbers in identifiers.
+    out = out.replace(/([a-zA-Z])(\d*)(\b;|\[)/g, '$1$3');
+    appendFile(`\n${out}`);
+  }
+  return prettier.format(output, {
+    parser: 'typescript',
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    tabWidth: 2,
+  });
+}
+
+if (argv.output) {
+  compileSchemas();
+}
